fb458c7a0fdccc0d6e2824c053b4b74e0315a728
gmakarov <german@appello.com.au>
2023-01-21 02:18:44 +0300

    setup package

---
+++ README.md
@@ -0,0 +1,59 @@
+# dot-path-value
+
+Safely get deep nested properties using dot notation.
+
+<a href="https://www.npmjs.com/package/dot-path-value">
+  <img alt="npm version" src="https://img.shields.io/npm/v/dot-path-value.svg?style=flat-square" />
+</a>
+<a href="https://www.npmjs.com/package/dot-path-value">
+  <img alt="npm downloads" src="https://img.shields.io/npm/dm/dot-path-value.svg?style=flat-square" />
+</a>
+<a href="https://bundlephobia.com/package/dot-path-value">
+  <img alt="npm minified bundle size" src="https://img.shields.io/bundlephobia/min/dot-path-value?style=flat-square">
+</a>
+<a href="https://bundlephobia.com/package/dot-path-value">
+  <img alt="npm gzip minified bundle size" src="https://img.shields.io/bundlephobia/minzip/dot-path-value?style=flat-square">
+</a>
+
+## Features
+
+- TypeScript first 🤙
+- Support arrays
+- Lightweight
+- No dependencies
+
+## Installation
+
+```bash
+# using npm
+npm install dot-path-value
+# using pnpm
+pnpm install dot-path-value
+# using yarn
+yarn add dot-path-value
+```
+
+## Usage
+
+```ts
+import { getByPath } from "dot-path-value";
+
+const obj = {
+  a: {
+    b: {
+      c: "hello",
+    },
+  },
+};
+
+getByPath(obj, "a.b.c"); // outputs 'hello'
+```
+
+## Types
+
+`dot-path-value` exports a few types to ensure the type safety:
+
+| Type                  | Description                                                                               |
+| --------------------- | ----------------------------------------------------------------------------------------- |
+| `Path<T>`             | converts nested structure `T` into a string representation of the paths to its properties |
+| `PathValue<T, TPath>` | returns the type of the value at the specified path                                       |
---
+++ src/index.ts
@@ -0,0 +1,82 @@
+export type Primitive =
+  | null
+  | undefined
+  | string
+  | number
+  | boolean
+  | symbol
+  | bigint;
+
+type ArrayKey = number;
+type IsTuple<T extends readonly any[]> = number extends T["length"]
+  ? false
+  : true;
+type TupleKeys<T extends readonly any[]> = Exclude<keyof T, keyof any[]>;
+
+export type PathConcat<
+  TKey extends string | number,
+  TValue
+> = TValue extends Primitive
+  ? `${TKey}`
+  : `${TKey}` | `${TKey}.${Path<TValue>}`;
+
+export type Path<T> = T extends readonly (infer V)[]
+  ? IsTuple<T> extends true
+    ? {
+        [K in TupleKeys<T>]-?: PathConcat<K & string, T[K]>;
+      }[TupleKeys<T>]
+    : PathConcat<ArrayKey, V>
+  : {
+      [K in keyof T]-?: PathConcat<K & string, T[K]>;
+    }[keyof T];
+
+type ArrayPathConcat<
+  TKey extends string | number,
+  TValue
+> = TValue extends Primitive
+  ? never
+  : TValue extends readonly (infer U)[]
+  ? U extends Primitive
+    ? never
+    : `${TKey}` | `${TKey}.${ArrayPath<TValue>}`
+  : `${TKey}.${ArrayPath<TValue>}`;
+
+export type ArrayPath<T> = T extends readonly (infer V)[]
+  ? IsTuple<T> extends true
+    ? {
+        [K in TupleKeys<T>]-?: ArrayPathConcat<K & string, T[K]>;
+      }[TupleKeys<T>]
+    : ArrayPathConcat<ArrayKey, V>
+  : {
+      [K in keyof T]-?: ArrayPathConcat<K & string, T[K]>;
+    }[keyof T];
+
+export type PathValue<T, TPath extends Path<T> | ArrayPath<T>> = T extends any
+  ? TPath extends `${infer K}.${infer R}`
+    ? K extends keyof T
+      ? R extends Path<T[K]>
+        ? PathValue<T[K], R>
+        : never
+      : K extends `${ArrayKey}`
+      ? T extends readonly (infer V)[]
+        ? PathValue<V, R & Path<V>>
+        : never
+      : never
+    : TPath extends keyof T
+    ? T[TPath]
+    : TPath extends `${ArrayKey}`
+    ? T extends readonly (infer V)[]
+      ? V
+      : never
+    : never
+  : never;
+
+export function getByPath<T extends Record<string, any>, TPath extends Path<T>>(
+  obj: T,
+  path: TPath
+): PathValue<T, TPath> {
+  return path.split(".").reduce((acc, key) => acc[key], obj) as PathValue<
+    T,
+    TPath
+  >;
+}
